f525fa8aeb1ebd58a7477c9fa218c5a5b2d55038
Ubuntu <ubuntu@ip-172-31-28-42.us-west-2.compute.internal>
2017-04-12 23:12:59 +0000

    Basic Local Web App Working

--- flaskapp/config.py
+++ flaskapp/config.py
@@ -1,26 +1,26 @@
-  class Config(object):
-    """
-    Common configurations
-    
-    """
+class Config(object):
+  """
+  Common configurations
+  
+  """
 
-  class DevelopmentConfig(Config):
-    """
-    Development Configurations
+class DevelopmentConfig(Config):
+  """
+  Development Configurations
 
-    """
+  """
 
-    DEBUG = True
-    SQLALCHEMY_ECHO = True
+  DEBUG = True
+  SQLALCHEMY_ECHO = True
 
-  class ProductionConfig(Config):
-    """
-    Production configurations
-    
-    """
-    DEBUG = False
+class ProductionConfig(Config):
+  """
+  Production configurations
+  
+  """
+  DEBUG = False
 
-  app_config = {
-    'development': DevelopmentConfig,
-    'production': PRoductionConfig
-  }
+app_config = {
+  'development': DevelopmentConfig,
+  'production': ProductionConfig
+}
